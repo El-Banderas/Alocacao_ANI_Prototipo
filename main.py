dba3b5fa73cc26dc4be831766b2267841251bc52
Diogo Manuel Brito Pires <30441515+El-Banderas@users.noreply.github.com>
2023-09-30 16:06:26 +0100

    Antes de fazer asneiras

--- main.py
+++ main.py
@@ -2,13 +2,15 @@ from input.input import read_input
 from solver.solver import main_solver
 from output.write_output_excel import write_output
 
-path_input_excel = './input/input.xlsm'
+path_input_excel = './input/Excel_input.xlsm'
 path_output_excel = './output/output.xlsx'
 
 if __name__ == '__main__':
     input = read_input(path_excel=path_input_excel)
     attribution = main_solver(input=input)
     write_output(output=attribution, excel_path=path_output_excel)
+    print("Conclusão da execução do programa")
+    print("Esta janela pode ser fechada.")
 
 
 
