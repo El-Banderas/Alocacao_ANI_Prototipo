c3ae05d757fb1916c2b71b453a1ce231f6a9622e
Diogo Manuel Brito Pires <30441515+El-Banderas@users.noreply.github.com>
2023-12-16 16:42:54 +0000

    Server mais arranjado, e combinar exceis

--- info_classes/Project.py
+++ info_classes/Project.py
@@ -9,12 +9,54 @@ class Proj:
         self.currentPhase = currentPhase
         self.analysis_tech = analysis_tech 
         self.other_tech = other_tech 
+    
+    # This value is readen in a different time
+    def add_name(self, name : str):
+        self.name = name
 
     def __str__(self):
         return f"{self.id} - {self.theme} (tema): cost {self.costAccomp} / {self.nProm} (num prom) and {self.currentPhase} (currentPhase) "
     
+    def addDates(self, init_date : str, end_date : str):
+        self.init_date = init_date
+        self.end_date = end_date
+    
     def convert_to_dict(self):
         return self.__dict__
-    
-    
 
+    def convert_state_str(self):
+        match self.currentPhase:
+            case 0:
+                return "Por analisar"
+            case 1:
+                return "Aprovado"
+            case 2:
+                return "Rejeitado"
+            case 3:
+                return "Concluído"
+            case _:
+                return "---"
+
+    def current_effort(self):
+        if self.currentPhase == 2:
+            return 0
+        if self.other_tech == None:
+            return self.costAnalysis
+        else:
+            return self.costAccomp
+
+    def convert_to_dict_pt(self):
+        dict_res = {}
+        dict_res["id"] = self.id
+        dict_res["Custo análise"] = self.costAnalysis
+        dict_res["Custo acompanhamento"] = self.costAccomp
+        dict_res["Tema"] = self.theme
+        dict_res["N. Promotores"] = self.nProm
+        dict_res["Fase atual"] = self.convert_state_str()
+        dict_res["Tecn. análise"] = self.analysis_tech
+        dict_res["Tecn. acompanhamento"] = self.other_tech
+        dict_res["Sigla"] = self.name
+        dict_res["Data início"] = self.init_date
+        dict_res["Data fim"] = self.end_date
+        dict_res["Esf. atual"] = self.current_effort()
+        return dict_res
--- server/server.py
+++ server/server.py
@@ -15,6 +15,7 @@ def get_attributions_from_input(projs : list[Proj], input_translations):
         attributions = {}
         for proj in projs:
             proj_name = input_translations["projs"][proj.id]
+            proj.add_name(proj_name)
             # Check analysis
             if proj.analysis_tech != None:
                 tecn_name = input_translations["tecns"][proj.analysis_tech]
@@ -57,12 +58,8 @@ class MyServer(BaseHTTPRequestHandler):
         tecn = list(filter(lambda tecn: tecn.id == tecn_id, self.tecns))
         if len(tecn) > 0:
             res = {}
-            res["info"] = tecn[0].convert_to_dict()
-            res["info"]["current_effort"] = int(res["info"]["current_effort"])
-            if res["info"]["availability"]:
-                res["info"]["availability"] = "Sim"
-            else: 
-                res["info"]["availability"] = "Não"
+            res["info"] = tecn[0].convert_to_dict_pt()
+            
             res["projects"] = []
             tecn_name = self.get_tecn_name_by_id(tecn_id=tecn_id)
             projs = self.attributions["technicians"][clean_name]
@@ -71,7 +68,8 @@ class MyServer(BaseHTTPRequestHandler):
 
                 proj_id = int(self.translations["projs"].index(unquote(proj_name)))
                 proj_info = list(filter(lambda this_proj: this_proj.id == proj_id, self.projs))[0]
-                res["projects"].append(proj_info.convert_to_dict())
+                res["projects"].append(proj_info.convert_to_dict_pt())
+            res["projects_tecn_id"] = tecn_id
             return res #tecn[0].convert_to_dict()
         else: 
             # DEBUG, no project found
@@ -87,7 +85,7 @@ class MyServer(BaseHTTPRequestHandler):
         
         proj = list(filter(lambda proj: proj.id == proj_id, self.projs))
         if len(proj) > 0:
-            return { "info": proj[0].convert_to_dict()}
+            return { "info": proj[0].convert_to_dict_pt()}
         else: 
             # DEBUG, no project found
             print("No proj: ", proj_id)
