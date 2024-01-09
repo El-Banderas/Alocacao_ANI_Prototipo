a33aeae2d854bfd0e1164edb263b4514fa069f38
Diogo Manuel Brito Pires <30441515+El-Banderas@users.noreply.github.com>
2024-01-09 18:22:17 +0000

    Update efforts projects

--- serverQueries/getProjectsForCosts.py
+++ serverQueries/getProjectsForCosts.py
@@ -5,8 +5,8 @@ def get_Projects_For_Costs(bd):
     res = []
     for project in projects_raw:
         id, sigla, name, id_topo,id_fase, id_area, data_init, data_end, effort_analisis, effort_accomp = project
-        area = bd.do_command(f"select Area from T_AREA_TEMATICA where Id_Area == {id_area} ")
-        tipo = bd.do_command(f"select Tipologia from T_TIPOLOGIA where Id_Tipologia == {id_topo}")
+        area = bd.do_command(f"select Area from T_AREA_TEMATICA where Id_Area == {id_area} ")[0][0]
+        tipo = bd.do_command(f"select Tipologia from T_TIPOLOGIA where Id_Tipologia == {id_topo}")[0][0]
 
         res.append({
             "id" : id,
